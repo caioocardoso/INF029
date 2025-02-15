f1c3245dcaabca7629628578adaeb87f3bb8fe6a
Caio Oliveira Cardoso <136840071+caioocardoso@users.noreply.github.com>
2025-02-14 21:18:58 -0300

    Create L3Q14.c

---
+++ Listas/L3Q14.c
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+void todosNaturais(int n);
+
+int main()
+{
+    todosNaturais(20);
+}
+
+void todosNaturais(int n){
+    if (n > 0) {
+        todosNaturais(n - 1);
+    }
+    printf("%d ", n);
+}
